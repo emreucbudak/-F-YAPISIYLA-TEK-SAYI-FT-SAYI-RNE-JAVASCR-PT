c056baf1c4653f1854e359fb0498ff95d495c23e
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2024-03-24 00:09:55 +0300

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,9 @@
+let i;
+for(i = 0 ; i<101 ; i++) {
+    if (i%2 != 0) {
+        document.writeln("<br>Tek Sayı Çıktı sayı = " +i +"<br>")
+    }
+    else {
+        document.writeln("<br>Çift Sayı Çıktı sayı = " +i +"<br>")
+    }
+}
